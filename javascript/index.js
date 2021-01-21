d1493f91c143704de0015ddac50e15f57dd1110f
Daniel May <danielmay113@gmail.com>
2021-01-21 16:55:53 +0000

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,28 @@
 // JS FILE FOR INDEX.HTML
 
+const addToCartButton = document.querySelectorAll(".buy-item");
+const shoppingCart = document.querySelector("#cart-items");
+const cartItem = document.querySelectorAll(".cart-item");
+
+function addCartItem(){
+
+    const newItem = document.createElement('li');
+    newItem.className = 'cart-item';
+
+    const newItemImage = document.createElement('img');
+    newItemImage.src = "images/gift-icon.png";
+    newItemImage.alt = "an icon of a blue gift box and ribbon";
+
+    const cancelItemImage = document.createElement('img');
+    cancelItemImage.className = "remove-button"
+    cancelItemImage.src = "images/cancel-icon.png";
+    cancelItemImage.alt = "red remove icon"
+
+    newItem.appendChild(newItemImage);
+    newItem.appendChild(cancelItemImage);
+
+    shoppingCart.appendChild(newItem)
+
+}
+
+addToCartButton.addEventListener('click', addCartItem());
